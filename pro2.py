735920ded4a7d3dfe0e0cec89b62d0d8be71668a
anjalikachhadiya <154410124+anjalikachhadiya@users.noreply.github.com>
2023-12-20 13:16:16 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -1,8 +1,13 @@
-#factorial number
+num = int(input("Enter a number: "))
 
-n = int(input(“Enter a number: “))
 factorial = 1
-if n >= 1:
-for i in range (1, n+1):
-factorial = factorial *i
-print (“Factorial of the given number is: “, factorial)
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
